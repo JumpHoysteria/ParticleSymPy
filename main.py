14cbdb62d9f1fafe7f511c88123efa2aa8985fa8
Alexandre Meier <alexandre_meier@bluewin.ch>
2024-07-23 14:49:00 +0200

    Update main.py
    Increased efficiency by evaluating all interactions only once (Newtons 2nd).

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import math
 # Press 'q': End Simulation
 # Press 'r': Chaos Mode
 # Press 'e': Populate Area
-# Press 'w': Increase Gravitational Constant (Double)
-# Press 's': Decrease Gravitational Constant (Half)
+# Press 'p': Toggle Gravitation/Repulsion
+# Press 'w': Increase Force Constant (Double)
+# Press 's': Decrease Force Constant (Half)
 # Press 't': Increase Friction (-10%)
 # Press 'g': Decrease Frcition (+10%)
 # Mouse, Left Click: Add Atom with Random Velocity
@@ -19,33 +20,32 @@ class Game:
         # initialising pygame
         self.MAX_FORCE= 9.81
         self.DISTANCE_CAP = 20
-        self.FRICTION = 0.3
+        self.FRICTION = 0.6
         self.SIZE_RECT = 8
-        self.TICK = 30
+        self.TICK = 60
 
-        pygame.init()
         self.width = width
-        self.v_abs_samples = []
         self.height = height
-        self.Particles = []
-        self.screen = pygame.display.set_mode((self.width, self.height))
-
-        pygame.display.set_caption("Alex Particle")
-        self.screen.fill(0)
         self.col_thresh = 5
-        pygame.display.update()
+        self.v_abs_samples = []
+        self.Particles = []
+        self.Particles_t1 = []
         self.running = running
 
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Alex Particle")
+        self.screen.fill(0)
+        pygame.display.update()
         self.clock = pygame.time.Clock()
 
-
     def addRandomSpeedParticle(self, x,y):
-        self.Particles.append([x,y,(random.random()-0.5)*20,(random.random()-0.5)*20])
+        self.Particles.append([x,y,(random.random()-0.5)*2*self.col_thresh,(random.random()-0.5)*2*self.col_thresh])
 
     def addParticle(self, x, y):
         self.Particles.append([x,y,0,0])
 
-    def createParticle(self, x, y, color):
+    def _createParticle(self, x, y, color):
         pygame.draw.rect(self.screen, color, (x, y, self.SIZE_RECT, self.SIZE_RECT))
 
     def getColorFromSpeed(self, vx,vy):
@@ -70,12 +70,12 @@ class Game:
             x = 1400*random.random()
             y = 1000*random.random()
             self.Particles.append([x,y,(random.random()-0.5)*20,(random.random()-0.5)*20])
-            self.createParticle(x, y, (200,200,200))  
+            self._createParticle(x, y, (200,200,200))  
 
         while self.running:
             # DRAW
             for particle in self.Particles:
-                self.createParticle(particle[0],particle[1],self.getColorFromSpeed(particle[2],particle[3]))
+                self._createParticle(particle[0],particle[1],self.getColorFromSpeed(particle[2],particle[3]))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -95,12 +95,13 @@ class Game:
                         bool_addFixParticle = False
                 elif not pygame.mouse.get_pressed()[2]:
                         bool_addFixParticle = True
+            self.Particles_t1 = [[0,0,0,0] for _ in range(len(self.Particles))]
             for i in range(len(self.Particles)):
                 (a_x, a_y, a_vx, a_vy) = self.Particles[i]
-                for j in range(len(self.Particles)):
+                for j in range(i, len(self.Particles)):
                     if i == j:
                         continue
-                    (b_x, b_y) = self.Particles[j][0], self.Particles[j][1]
+                    (b_x, b_y, b_vx, b_vy) = self.Particles[j]
                     dx = b_x - a_x
                     dy = b_y - a_y
                     d = math.sqrt(dx**2 + dy**2)
@@ -112,22 +113,34 @@ class Game:
 
                         fx = (dx / d) * F
                         fy = (dy / d) * F
+                        b_vx -= fx
+                        b_vy -= fy
+                        b_x -= b_vx
+                        b_y -= b_vy
+
                         a_vx += fx
                         a_vy += fy
                         a_x += a_vx
-                        a_y += a_vy
+                        a_y += a_vy                       
 
-                        self.Particles[i][0] = a_x
-                        self.Particles[i][1] = a_y
-                        self.Particles[i][2] = a_vx*self.FRICTION
-                        self.Particles[i][3] = a_vy*self.FRICTION
+                        self.Particles_t1[i][0] = a_x
+                        self.Particles_t1[i][1] = a_y
+                        self.Particles_t1[i][2] = a_vx*self.FRICTION
+                        self.Particles_t1[i][3] = a_vy*self.FRICTION
+                        self.Particles_t1[j][0] = b_x
+                        self.Particles_t1[j][1] = b_y
+                        self.Particles_t1[j][2] = b_vx*self.FRICTION
+                        self.Particles_t1[j][3] = b_vy*self.FRICTION
+
+                if self.Particles[i][0] >= self.width or self.Particles[i][0] <= 0:
+                    self.Particles_t1[i][2] *= (-1)
+                    self.Particles_t1[i][0] = self.width*round(self.Particles[i][0]/self.width)*0.99+5
+                if self.Particles[i][1] >= self.height or self.Particles[i][1] <= 0:
+                    self.Particles_t1[i][3] *= (-1)
+                    self.Particles_t1[i][1] = self.height*round(self.Particles[i][1]/self.height)*0.99+5
 
 
-                        if self.Particles[i][0] >= 1400 or self.Particles[i][0] <= 0:
-                            self.Particles[i][2] *= (-1)
-                        if self.Particles[i][1] >= 1000 or self.Particles[i][1] <= 0:
-                            self.Particles[i][3] *= (-1)
-
+            self.Particles = self.Particles_t1
             self.col_thresh = max(self.v_abs_samples)*0.9
             # updating the display
             pygame.display.update()
@@ -140,24 +153,31 @@ class Game:
                             x = 1400*random.random()
                             y = 1000*random.random()
                             self.Particles.append([x,y,(random.random()-0.5)*20,(random.random()-0.5)*20])
-                            self.createParticle(x, y, (200,200,200))  
+                            self._createParticle(x, y, (200,200,200))  
                     elif pygame.key.name(event.key) == 'e':
                         self.Particles = []
-                        for _ in range(5):
+                        for _ in range(10):
                             x = 1400*random.random()
                             y = 1000*random.random()
                             self.Particles.append([x,y,(random.random()-0.5)*20,(random.random()-0.5)*20])
-                            self.createParticle(x, y, (200,200,200))  
+                            self._createParticle(x, y, (200,200,200))  
                     elif pygame.key.name(event.key) == 'q':
                         self.running = not self.running
+                    elif pygame.key.name(event.key) == 'p':
+                                self.MAX_FORCE *= (-1)
+                                print(f"Force inverted: {self.MAX_FORCE}")
                     elif pygame.key.name(event.key) == 'w':
                                 self.MAX_FORCE *= 2
+                                print(f"Max Force: {self.MAX_FORCE}")
                     elif pygame.key.name(event.key) == 's':
                                 self.MAX_FORCE *= 1/2
+                                print(f"Max Force: {self.MAX_FORCE}")
                     elif pygame.key.name(event.key) == 't': 
                                 self.FRICTION += 0.1 
+                                print(f"Friction: {self.FRICTION}")
                     elif pygame.key.name(event.key) == 'g':
                                 self.FRICTION -= 0.1
+                                print(f"Friction: {self.FRICTION}")
             self.screen.fill(0)
         pygame.quit()
 
